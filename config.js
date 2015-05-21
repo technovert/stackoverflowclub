bec6706092aa1df523f8efb35341ee9927e43800
chandermani <chandermani@outlook.com>
2015-05-21 16:39:48 +0530

    Moved the question loading to stackoverflow client.

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ System.config({
     "aurelia-bootstrapper": "github:aurelia/bootstrapper@0.12.0",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
     "aurelia-framework": "github:aurelia/framework@0.11.0",
-    "aurelia-http-client": "github:aurelia/http-client@0.8.0",
+    "aurelia-http-client": "github:aurelia/http-client@0.8.1",
     "aurelia-router": "github:aurelia/router@0.8.0",
     "babel": "npm:babel-core@5.4.3",
     "babel-runtime": "npm:babel-runtime@5.4.3",
     "bootstrap": "github:twbs/bootstrap@3.3.4",
-    "core-js": "npm:core-js@0.9.7",
+    "core-js": "npm:core-js@0.9.11",
     "css": "github:systemjs/plugin-css@0.1.10",
     "font-awesome": "npm:font-awesome@4.3.0",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
@@ -38,7 +38,7 @@ System.config({
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
       "aurelia-metadata": "github:aurelia/metadata@0.5.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/bootstrapper@0.12.0": {
       "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.4.0",
@@ -52,12 +52,12 @@ System.config({
       "aurelia-templating-binding": "github:aurelia/templating-binding@0.11.0",
       "aurelia-templating-resources": "github:aurelia/templating-resources@0.11.1",
       "aurelia-templating-router": "github:aurelia/templating-router@0.12.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/dependency-injection@0.7.1": {
       "aurelia-logging": "github:aurelia/logging@0.4.0",
       "aurelia-metadata": "github:aurelia/metadata@0.5.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/framework@0.11.0": {
       "aurelia-binding": "github:aurelia/binding@0.6.1",
@@ -68,15 +68,15 @@ System.config({
       "aurelia-path": "github:aurelia/path@0.6.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
       "aurelia-templating": "github:aurelia/templating@0.11.2",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/history-browser@0.4.0": {
       "aurelia-history": "github:aurelia/history@0.4.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
-    "github:aurelia/http-client@0.8.0": {
+    "github:aurelia/http-client@0.8.1": {
       "aurelia-path": "github:aurelia/path@0.6.1",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/loader-default@0.7.0": {
       "aurelia-loader": "github:aurelia/loader@0.6.0",
@@ -85,14 +85,14 @@ System.config({
     "github:aurelia/loader@0.6.0": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
       "aurelia-path": "github:aurelia/path@0.6.1",
-      "core-js": "npm:core-js@0.9.7",
+      "core-js": "npm:core-js@0.9.11",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.1"
     },
     "github:aurelia/metadata@0.5.0": {
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/route-recognizer@0.4.0": {
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/router@0.8.0": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
@@ -100,7 +100,7 @@ System.config({
       "aurelia-history": "github:aurelia/history@0.4.0",
       "aurelia-path": "github:aurelia/path@0.6.1",
       "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.4.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/templating-binding@0.11.0": {
       "aurelia-binding": "github:aurelia/binding@0.6.1",
@@ -113,7 +113,7 @@ System.config({
       "aurelia-logging": "github:aurelia/logging@0.4.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
       "aurelia-templating": "github:aurelia/templating@0.11.2",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:aurelia/templating-router@0.12.0": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.7.1",
@@ -131,7 +131,7 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.5.0",
       "aurelia-path": "github:aurelia/path@0.6.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.4.0",
-      "core-js": "npm:core-js@0.9.7"
+      "core-js": "npm:core-js@0.9.11"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -212,8 +212,10 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.9.7": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:core-js@0.9.11": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:core-util-is@1.0.1": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
--- src/home.js
+++ src/home.js
@@ -1,4 +1,7 @@
-import {inject} from 'aurelia-framework';
+import {
+  inject
+}
+from 'aurelia-framework';
 import {
   computedFrom
 }
@@ -7,63 +10,21 @@ import {
   Router
 }
 from 'aurelia-router';
-@inject(Router)
+import {
+  SOClient
+}
+from './stackoverflowclient'
+
+@
+inject(Router, SOClient)
 export class Home {
-  constructor(Router) {
-    this.router = Router;
-    this.questions.push(new Question({
-      "tags": [
-        "android",
-        "json",
-        "base64"
-      ],
-      "owner": {
-        "reputation": 1,
-        "user_id": 4181287,
-        "user_type": "registered",
-        "profile_image": "https://www.gravatar.com/avatar/e2eb71a2c3c0a875f22f1c724fb345ef?s=128&d=identicon&r=PG&f=1",
-        "display_name": "don",
-        "link": "http://stackoverflow.com/users/4181287/don"
-      },
-      "answered": false,
-      "viewCount": 15,
-      "answerCount": 0,
-      "score": -1,
-      "lastActivityDate": 1431782565 * 1000,
-      "creationDate": 1431781521 * 1000,
-      "lastEditDate": 1431782565 * 1000,
-      "id": 30276022,
-      "link": "http://stackoverflow.com/questions/30276022/bitmapfactory-decodebytearray-returning-null",
-      "title": "BitmapFactory.decodeByteArray returning null"
-    }));
-    this.questions.push(new Question({
-      "tags": [
-        "node.js",
-        "rest",
-        "loopbackjs",
-        "strongloop"
-      ],
-      "owner": {
-        "reputation": 1,
-        "user_id": 4751669,
-        "user_type": "registered",
-        "profile_image": "http://graph.facebook.com/10206446861594807/picture?type=large",
-        "display_name": "Yoshihisa Sato",
-        "link": "http://stackoverflow.com/users/4751669/yoshihisa-sato"
-      },
-      "answered": false,
-      "viewCount": 16,
-      "answerCount": 0,
-      "score": 0,
-      "lastActivityDate": 1431782561 * 1000,
-      "creationDate": 1431680574 * 1000,
-      "lastEditDate": 1431782561 * 1000,
-      "id": 30255524,
-      "link": "http://stackoverflow.com/questions/30255524/loopback-rest-findbyid-doesnt-work-well",
-      "title": "Loopback REST findById doesn&#39;t work well"
-    }));
-  }
   questions = [];
+  constructor(Router, SOClient) {
+    this.router = Router;
+    this.soClient = SOClient;
+    this.questions = this.soClient.loadRecentQuestions();
+  }
+
   //Getters can't be observed with Object.observe, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
   //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
@@ -77,12 +38,6 @@ export class Home {
   }
   goto(questionDetails) {
     this.router.navigate(`questions/${questionDetails}`);
-  }
-}
-
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
   }
 }
 
@@ -114,23 +69,4 @@ export class TimeAgoValueConverter {
     }
     return Math.floor(seconds) + " seconds";
   }
-}
-
-export class Question {
-  constructor(args) {
-    this.tags = args.tags;
-    this.owner = args.owner;
-    this.answered = args.answered;
-    this.score = args.score;
-    this.viewCount = args.viewCount;
-    this.answerCount = args.answerCount;
-    this.lastActivityDate = args.lastActivityDate;
-    this.creationDate = args.creationDate;
-    this.id = args.id;
-    this.lastEditDate = args.lastEditDate;
-    this.link = args.link;
-    this.title = args.title;
-    this.edited = args.creationDate == args.lastEditDate ? false : true;
-  }
-
 }
