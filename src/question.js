92c634c947ebdc187ff8738581aa275ebf067d93
chandermani <chandermani@outlook.com>
2015-05-22 20:06:30 +0530

    Temp checkin

--- src/question.js
+++ src/question.js
@@ -1,5 +1,19 @@
+import {
+  inject
+}
+from 'aurelia-framework';
+import {
+  Router
+}
+from 'aurelia-router';
+
+@inject(Router)
 export class Question {
   constructor(Router) {
 
+  }
+
+  activate(params, routeConfig) {
+    console.log(`Question with ${params.id} loaded.`);
   }
 }
